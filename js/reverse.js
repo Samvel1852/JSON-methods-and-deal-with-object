add626300487417081afeeedfabc7b39d5d09457
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-14 15:00:49 +0400

    build: Created headTail.js file

--- js/reverse.js
+++ js/reverse.js
@@ -8,7 +8,9 @@ let fromPairs = [
 function reverseFn(array) {
   let reversedObj = {};
   for (let item in array) {
+    const [head, ...tail] = item;
     reversedObj[array[item][0]] = array[item][1];
+    // console.log(reversedObj[head]);
   }
   return reversedObj;
 }
