2195e40b0e98aed524900aa70df381d1b472f381
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-14 16:40:27 +0400

    fix: Fixed the function for reduce

--- js/groupBySomeRule.js
+++ js/groupBySomeRule.js
@@ -5,29 +5,30 @@ function groupBy(array, fn) {
 
   //by reduce
 
-  //   let resArr = array.map((item) => fn(item));
-  //   let resObj = array.reduce((acc, item, index) => {
-  //     let key = fn(item);
-  //     // console.log(key, item, "first");
-  //     if (acc.hasOwnProperty(key)) {
-  //       acc[key].push(item);
-  //     } else {
-  //       acc[key] = [item];
-  //       //   console.log(acc[key], "second");
-  //     }
-  //   }, {});
+  // let resArr = array.map((item) => fn(item));
+  let resObj = array.reduce((acc, item, index) => {
+    let key = fn(item);
+    // console.log(key, item, "first");
+    if (acc.hasOwnProperty(key)) {
+      acc[key].push(item);
+    } else {
+      acc[key] = [item];
+      //   console.log(acc[key], "second");
+    }
+    return acc;
+  }, {});
 
   // by forEach
 
-  array.forEach((element, index) => {
-    if (resObject.hasOwnProperty(fn(element))) {
-      resObject[fn(element)].push(element);
-    } else {
-      resObject[fn(element)] = [element];
-    }
-  });
-  return resObject;
+  //   array.forEach((element, index) => {
+  //     if (resObject.hasOwnProperty(fn(element))) {
+  //       resObject[fn(element)].push(element);
+  //     } else {
+  //       resObject[fn(element)] = [element];
+  //     }
+  //   });
+  return resObj;
 }
 
-console.log(groupBy([6.1, 4.2, 6.3], Math.floor));
+console.log(groupBy(["one", "two", "three"], (i) => i.length));
 // console.log();
